07e1a5bfa055a194e6e4c27a92a7c73014a536df
rallysport206 <phan.tony125@gmail.com>
2018-05-21 21:52:51 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="http://yuendi.com/uploads/3/5/3/9/35397959/1475808987.jpg" className="App-logo" alt="logo" />
+        </header>
+        <p className="App-intro">
+          
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
